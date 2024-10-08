028ffb40b68aa92ce50e76ddcdba39375964ee30
Santiago Olguín <SantoII1735@gmail.com>
2024-10-08 16:40:58 +0000

    added /api/translate/ route

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,42 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      if (req.body.text == "") {
+        res.json({ error: 'No text to translate' });
+        return;
+      } else if (!req.body.text  || !req.body.locale) {
+        res.json({ error: 'Required field(s) missing' });
+        return;
+      }
+
+      let text = req.body.text;
+      let locale = req.body.locale;
+      let translation;
+
+
+      switch(locale) {
+        case "american-to-british":
+          translation = translator.americanToBritish(text);
+          break;
+        case "british-to-american":;
+          translation = translator.britishToAmerican(text);
+          break;
+        default:
+        	res.json({ error: 'Invalid value for locale field' })
+          return;
+      }
+
+      if (text === translation) {
+        res.json({
+          text,
+          translation: "Everything looks good to me!"
+        });
+        return;
+      } else {
+        res.json({
+          text,
+          translation
+        });
+      }
     });
 };
