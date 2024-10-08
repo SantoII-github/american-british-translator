1143371b00da24284eaeaff069603d4cf2842814
Santiago Olguín <SantoII1735@gmail.com>
2024-10-08 21:59:45 +0000

    added redundant unit tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -25,4 +25,24 @@ suite('Unit Tests', () => {
             });
         }
     });
+
+    // These are redundant, but necessary to pass fCC's tests.
+    suite('Highlight translation tests', () => {
+        test('Highlight translation: "Mangoes are my favorite fruit."', (done) => {
+          assert.equal(translator.americanToBritish('Mangoes are my favorite fruit.'), 'Mangoes are my <span class="highlight">favourite</span> fruit.');
+          done();
+        });
+        test('Highlight translation: "I ate yogurt for breakfast."', (done) => {
+          assert.equal(translator.americanToBritish('I ate yogurt for breakfast.'), 'I ate <span class="highlight">yoghurt</span> for <span class="highlight">brekky</span>.');
+          done();
+        });
+        test('Highlight translation: "We watched the footie match for a while."', (done) => {
+          assert.equal(translator.britishToAmerican('We watched the footie match for a while.'), 'We watched the <span class="highlight">soccer</span> match for a while.');
+          done();
+        });
+        test('Highlight translation: "Paracetamol takes up to an hour to work."', (done) => {
+          assert.equal(translator.britishToAmerican('Paracetamol takes up to an hour to work.'), '<span class="highlight">Tylenol</span> takes up to an hour to work.');
+          done();
+        });
+      });
 });
