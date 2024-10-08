b1497cd2d3bda1b5d6f0d1310d7afc3c0eb6e8eb
Santiago Olguín <SantoII1735@gmail.com>
2024-10-08 14:46:06 +0000

    translator first version

--- components/translator.js
+++ components/translator.js
@@ -4,7 +4,49 @@ const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
 class Translator {
+    #reverseTranslations(translationFile) {
+        // Create a new object by swapping keys and values
+        const reversed = {};
+        for (let key in translationFile) {
+            reversed[translationFile[key]] = key;
+        }
+        return reversed;
+      }
 
+    #translateAmerican = [
+        americanOnly,
+        this.#reverseTranslations(britishOnly),
+        americanToBritishSpelling,
+        americanToBritishTitles
+        ]
+
+    #translateBritish = [
+        this.#reverseTranslations(americanOnly),
+        britishOnly,
+        this.#reverseTranslations(americanToBritishSpelling),
+        this.#reverseTranslations(americanToBritishTitles)
+        ]
+
+    #translate(text, translationsArray)  {
+        let translation = text;
+
+        translationsArray.forEach(translationObj => {
+            for (let key in translationObj) {
+                const regex = new RegExp(`\\b${key}\\b`, 'g');
+                translation = translation.replace(regex, translationObj[key]);
+            }
+        })
+
+        return translation;
+    }
+
+    americanToBritish(text) {
+        return this.#translate(text, this.#translateAmerican);
+    }
+
+    britishToAmerican(text) {
+        return this.#translate(text, this.#translateBritish);
+    }
 }
 
 module.exports = Translator;
